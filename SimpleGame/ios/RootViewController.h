8b151c675fc1fc82ec7003d23520e6f800bb3857
StaticGa <staticga@gmail.com>
2013-05-22 22:56:42 +0800

    Initial Commit

---
+++ SimpleGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SimpleGameAppController.h
+//  SimpleGame
+//
+//  Created by Static Ga on 13-5-22.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ SimpleGame/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SimpleGame
+//
+//  Created by Static Ga on 13-5-22.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
